12617ce4d20e0ce9ecb066456d3b628c9885bf0f
oleg dashevskii <olegdashevskii@gmail.com>
2014-05-14 22:58:52 +0700

    Upgrade used clojars

--- project.clj
+++ project.clj
@@ -1,21 +1,21 @@
 (defproject trelltale "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [compojure "1.1.6"]
-                 [hiccup "1.0.4"]
-                 [environ "0.4.0"]
-                 [clojurewerkz/route-one "1.0.0"]
-                 [lib-noir "0.8.0"]
-                 [http-kit "2.1.16"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [compojure "1.1.8"]
+                 [hiccup "1.0.5"]
+                 [environ "0.5.0"]
+                 [clojurewerkz/route-one "1.1.0"]
+                 [lib-noir "0.8.2"]
+                 [http-kit "2.1.18"]
                  [cheshire "5.3.1"]
-                 [com.taoensso/timbre "3.0.1"]
-                 [ring/ring-json "0.2.0"]
-                 [clj-time "0.6.0"]
-                 [ring-server "0.3.0"]]
+                 [com.taoensso/timbre "3.2.1"]
+                 [ring/ring-json "0.3.1"]
+                 [clj-time "0.7.0"]
+                 [ring-server "0.3.1"]]
   :plugins [[lein-ring "0.8.7"]]
   :ring {:handler trelltale.handler/app
-         :init trelltale.handler/init
+         :init    trelltale.handler/init
          :destroy trelltale.handler/destroy}
   :aot :all
   :main trelltale.repl
@@ -24,4 +24,4 @@
    {:ring
     {:open-browser? false, :stacktraces? false, :auto-reload? false}}
    :dev
-   {:dependencies [[ring-mock "0.1.5"] [org.clojure/tools.namespace "0.2.4"] [ring/ring-devel "1.2.0"]]}})
+   {:dependencies [[ring-mock "0.1.5"] [org.clojure/tools.namespace "0.2.4"] [ring/ring-devel "1.2.2"]]}})
